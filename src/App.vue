4cb951d08f8cf1e30896925fedfd604ca72d46e3
Wekios <veljkod.blagojevic@gmail.com>
2018-11-07 16:11:11 +0100

    dynamic menu. right before routing

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,45 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="sideNav" fixed app>
+      <v-list>
+        <v-list-tile v-for="item in menuItems" :key="item.title">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="deep-orange darken-3">
+      <v-toolbar-side-icon 
+        @click.native.stop="sideNav = !sideNav"
+        class="hidden-sm-and-up" />
+      <v-toolbar-title>Diwanee Eats</v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items class="hidden-xs-only" >
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
-  }
-}
+  data: () => ({
+    sideNav: false,
+    menuItems: [
+      { icon: 'fastfood', title: "View Lunch Options" },
+      { icon: 'store', title: "Suggest place" },
+      { icon: 'person', title: "Profile" },
+      { icon: 'face', title: "Sign up" },
+      { icon: 'lock_open', title: "Sign in" }
+    ]
+  })
+};
 </script>
+
+<style lang="scss" scoped>
+</style>
